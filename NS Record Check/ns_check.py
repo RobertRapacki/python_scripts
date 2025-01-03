8b2fc0e1bfe43bace519447d763a3139bb2aebd2
RobertRapacki <135027389+RobertRapacki@users.noreply.github.com>
2025-01-03 14:09:41 -0500

    Create ns_check.py

---
+++ NS Record Check/ns_check.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import dns.resolver,re,sys
+
+def get_ns_record(domain):
+
+    resolver = dns.resolver.Resolver()
+
+    # Specify the nameserver(s)
+    resolver.nameservers = ['1.1.1.1']  # Cloudflare public DNS server
+
+    # Perform a DNS query
+    result = resolver.resolve(domain, 'NS')
+
+    # Prints the NS records
+    for rdata in result:
+         print(rdata)
+
+domain = sys.argv[1]
+
+get_ns_record(domain)
